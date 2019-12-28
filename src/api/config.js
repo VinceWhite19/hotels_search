99f171d66f2025f8229fa9fa22d812da530b0069
Vincent White <vincent19white@gmail.com>
2019-12-28 16:54:17 +0200

    Keys update

--- src/api/config.js
+++ src/api/config.js
@@ -1,4 +1,4 @@
-export const clientId = "puszzP3mxJThFXlB52bxTYUVeHD6yfeH";
-export const clientSecret = "HR3ZUuu6Z0sxqpMd";
-export const googleKey = "AIzaSyCsxHxAHH31l1hfzgK5Yu7oTqdbRtQB7ro";
-export const weatherKey = "7c9349ddaf6c8557740b40c2bfa2c0ea";
+export const clientId = "XXXXX";
+export const clientSecret = "XXX";
+export const googleKey = "XXXXX";
+export const weatherKey = "XXXX";
